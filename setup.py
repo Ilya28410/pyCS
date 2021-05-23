245471feb2c455118f68b2f6d57150a5bcc02655
Ilya28410 <84670924+Ilya28410@users.noreply.github.com>
2021-05-23 13:45:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
   packages = ['pyCS'],   # Chose the same as "name"
   version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = '',   # Give a short description about your library
+  description = 'Helps solve CS problems with python',   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/Ilya28410/pyCS',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Ilya28410/pyCS/archive/refs/tags/v_011.zip',    # I explain this later on
   keywords = ['CODE','COMPUTER', 'CS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'hopcroftkarp'
